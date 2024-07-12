ec5db44234d4f2da430fa8f7b61a652d85901982
Min Set <minn.set@gmail.com>
2024-07-12 13:44:55 +0700

    Finished Home Screen and redirected to sign-in

---
+++ constants/images.js
@@ -0,0 +1,6 @@
+
+import logo from "../assets/images/dynasty.png";
+import path from "../assets/images/path.png";
+import cargo from "../assets/images/cargo.png";
+
+export default { logo , cargo, path};
